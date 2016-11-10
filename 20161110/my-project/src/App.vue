46509c0d7a1937d8ff526e19285f7a64e488bb9f
fengnovo <fengnoku@gmail.com>
2016-11-10 20:56:25 +0800

    提交vue练习

---
+++ 20161110/my-project/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1 v-text="title"></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  title: 'this is a list',
+  items: [{
+    lable: 'coding',
+    isFinish: false
+  },{
+    lable: 'walking',
+    isFinish: true
+  }]
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
