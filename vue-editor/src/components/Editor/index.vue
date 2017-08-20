8b64db7d268312aa91c0a090096a6c62ca96cf09
fengnovo <fengnoku@126.com>
2017-08-20 23:41:17 +0800

    tinymce Editor test

---
+++ vue-editor/src/components/Editor/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="components-container">
+    <p>cms系统富文本,Tinymce<a target='_blank' href='https://segmentfault.com/a/1190000009762198#articleHeader13'> 相关文章 </a></p>
+    <div>
+      <Tinymce :height=200 ref="editor" v-model="content"></Tinymce>
+    </div>
+    <div class='editor-content' v-html='content'></div>
+  </div>
+</template>
+
+<script>
+  import Tinymce from './Editor';
+
+  export default {
+    components: { Tinymce },
+    data() {
+      return {
+        content: 'Tinymce'
+      }
+    }
+  };
+</script>
+
+<style scoped>
+.editor-content{
+  margin-top: 20px;
+}
+</style>
---
+++ vue-editor/src/components/Editor/indexs.js
@@ -0,0 +1,89 @@
+tinymce.init({
+selector: 'textarea',
+	height: 500,
+	content_css: [
+		'//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+		'//www.tinymce.com/css/codepen.min.css'
+	],
+
+	style_formats: [{
+			title: 'Bold text',
+			inline: 'strong'
+		},
+		{
+			title: 'Red text',
+			inline: 'span',
+			styles: {
+				color: '#ff0000'
+			}
+		},
+		{
+			title: 'Red header',
+			block: 'h1',
+			styles: {
+				color: '#ff0000'
+			}
+		},
+		{
+			title: 'Badge',
+			inline: 'span',
+			styles: {
+				display: 'inline-block',
+				border: '1px solid #2276d2',
+				'border-radius': '5px',
+				padding: '2px 5px',
+				margin: '0 2px',
+				color: '#2276d2'
+			}
+		},
+		{
+			title: 'Table row 1',
+			selector: 'tr',
+			classes: 'tablerow1'
+		}
+	],
+	formats: {
+		alignleft: {
+			selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img',
+			classes: 'left'
+		},
+		aligncenter: {
+			selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img',
+			classes: 'center'
+		},
+		alignright: {
+			selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img',
+			classes: 'right'
+		},
+		alignfull: {
+			selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img',
+			classes: 'full'
+		},
+		bold: {
+			inline: 'img',
+			image: 'http:////img3.tbcdn.cn/tfscom/TB1DrD_HpXXXXX5XVXXSutbFXXX.jpg_100x100.jpg'		},
+		italic: {
+			inline: 'span',
+			'classes': 'italic'
+		},
+		underline: {
+			inline: 'span',
+			'classes': 'underline',
+			exact: true
+		},
+		strikethrough: {
+			inline: 'del'
+		},
+		customformat: {
+			inline: 'span',
+			styles: {
+				color: '#00ff00',
+				fontSize: '20px'
+			},
+			attributes: {
+				title: 'My custom format'
+			},
+			classes: 'example1'
+		},
+	}
+});
