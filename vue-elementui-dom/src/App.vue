dc6cdd860f2bf86aca15b17f0c513c9b01c6bdce
fengnovo <fengnovo@users.noreply.github.com>
2018-01-25 00:18:11 +0800

    element createElement test

--- vue-elementui-dom/src/App.vue
+++ vue-elementui-dom/src/App.vue
@@ -2,11 +2,18 @@
     <div>  
         <input type="text" v-model='componentName'>
         <button @click='add'>click me to add a component</button>
+        <br />
+        <button @click='edit'>弹框</button>
+        <br />
+        <button @click='alertMe'>弹框alert</button>
+        <br />
+        <InputComponent a='sdfsdf'></InputComponent>
     </div> 
 </template>
 
  
 <script>  
+    import InputComponent from '@/components/InputComponent.vue';
     export default {
         data() {
             return {
@@ -14,11 +21,77 @@
                 componentName: ''
             }
         },  
+        components: { 
+            InputComponent, 
+            component1: { //第一个子组件  
+                template: "<div>这里是子组件1</div>"  
+            },  
+            component2: { //第二个子组件  
+                template: "<div>这里是子组件2</div>"  
+            } 
+        } ,
         methods: {
             add: function() {
                 this.allComponents.push(this.componentName)
                 // 重置输入框
                 this.componentName = ''
+            },
+            edit: function() {
+                this.$prompt('请输入邮箱', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    inputPattern: /[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?/,
+                    inputErrorMessage: '邮箱格式不正确'
+                }).then(({ value }) => {
+                    this.$message({
+                        type: 'success',
+                        message: '你的邮箱是: ' + value
+                    });
+                }).catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '取消输入'
+                    });       
+                });
+            },
+            onChange(data) {
+                console.log(data);
+            },
+            alertMe: function() {
+                const h = this.$createElement;
+                let _this = this;
+                let close = false;
+                this.$alert('请输入邮箱', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    message: h('div', null, [
+                        h(InputComponent, {
+                            props: {
+                                a: 'foo',
+                                close
+                            },
+                            on: {
+                                onChange: _this.onChange
+                            },
+                            ref: 'myRef'
+                        })
+                    ]),
+                    beforeClose: (action, instance, done) => {
+                        close = true;
+                        done();
+                    }
+                }).then((action, instance, done) => {
+                    this.$message({
+                        type: 'success',
+                        message: '你'
+                    });
+                    console.log(action, instance, done);
+                }).catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '取消输入'
+                    });       
+                });
             },
             render: function(h) { // h 为 createElement 函数，接受三个参数
                 // tag 
@@ -28,14 +101,6 @@
                     return h(componentName)
                 }))
             }
-        },  
-        components: {  
-            component1: { //第一个子组件  
-                template: "<div>这里是子组件1</div>"  
-            },  
-            component2: { //第二个子组件  
-                template: "<div>这里是子组件2</div>"  
-            } 
-        }  
+        } 
     } 
 </script>  
---
+++ vue-elementui-dom/src/components/InputComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p>{{a}}</p>
+    <el-input v-model="input" placeholder="请输入内容"></el-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'input-component',
+  props: ['a','onChange','close'],
+  data() {
+    return {
+      input: ''
+    }
+  },
+  watch: {
+    input: function() {
+      // this.$emit('onChange', this.input);
+    },
+    close: function(newVal,oldVal) {
+      console.log(newVal);
+      if (val) {
+        this.$emit('onChange', this.input);
+      }
+    },
+  },
+  created() {
+    console.log(this.close);
+  },
+  method: {
+    getData() {
+      return this.input;
+    }
+  }
+}
+</script>
