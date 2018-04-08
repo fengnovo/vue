a714faf675ee9e64774e446b05b4a55d7d5cd3f5
huangyunfeng <huangyunfeng@jd.com>
2018-04-08 21:53:03 +0800

    vue-book

---
+++ vue-book/src/base/Banner.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="footer">
+        <router-link to="home">home</router-link>
+        <router-link to="list">list</router-link>
+        <router-link to="collection">collection</router-link>
+        <router-link to="add">add</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Tab',
+  data () {
+    return {
+      msg: 'Tab'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.footer {
+   position: fixed;
+   bottom: 0;
+   right: 0;
+   left: 0;
+   display: flex;
+   width: 100%;
+   height: 50px;
+
+}
+</style>
---
+++ vue-book/src/base/Tab.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="footer">
+        <router-link to="home"><i class="iconfont icon-zhuye"></i></router-link>
+        <router-link to="list"><i class="iconfont icon-yewubaobiao"></i></router-link>
+        <router-link to="collection"><i class="iconfont icon-collection-b"></i></router-link>
+        <router-link to="add"><i class="iconfont icon-tianjia"></i></router-link>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Tab',
+  data () {
+      return {
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+@import '../assets/icon.css';
+.footer {
+     position: fixed;
+     bottom: 0;
+     right: 0;
+     left: 0;
+     width: 100%;
+     height: 50px;
+     display: flex;
+     border-top: 1px solid #ccc;
+     a {
+         flex: 1;
+         flex-direction: row;
+         justify-content: center;
+         align-items: center;
+         font-size: 30px;
+         line-height: 50px;
+     }
+
+}
+
+</style>
