6dc1780e9cea216316bc0fbe9dbf0a896fe25d81
huangyunfeng <huangyunfeng@jd.com>
2018-04-29 14:53:03 +0800

    ele2

---
+++ ele2/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <SelectContainer />
+        <TableContainer />
+    </div>
+</template>
+
+<script>
+import TableContainer from './components/TableContainer.vue';
+import SelectContainer from './components/SelectContainer.vue';
+export default {
+    name: 'App',
+    components: {
+       TableContainer,
+       SelectContainer
+    }
+}
+</script>
+
+<style>
+.el-form-item__label {
+    font-size: 12px;
+}
+</style>
